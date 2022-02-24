73092e93bed0e1c00cfccc738a1b5160903cbfa9
Ben Mora <blackopsben@gmail.com>
2022-02-24 12:50:40 -0600

    stop crying

--- Assets/Enemies/EyeGuyDeathBehavior.cs
+++ Assets/Enemies/EyeGuyDeathBehavior.cs
@@ -6,10 +6,13 @@ public class EyeGuyDeathBehavior : DefaultDeathBehavior
 {
     [SerializeField] private GameObject eyePrefab;
     [SerializeField] private Transform eyeSpawnTransform;
+    [SerializeField] private ParticleSystem cryPFXtoStop;
 
     public override void Die()
     {
         Instantiate(eyePrefab, eyeSpawnTransform.position, Quaternion.identity);
+
+        cryPFXtoStop.Stop();
 
         base.Die();
     }
